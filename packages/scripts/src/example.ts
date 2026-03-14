b809141688c9e3bd1c72bab0b4026c5ad3dbb264
Briony Frater <briony.frater@particularpresence.com>
2026-03-14 14:33:40 -0500

    init

--- packages/scripts/src/example.ts
+++ packages/scripts/src/example.ts
@@ -1,4 +1,4 @@
 import { Resource } from "sst";
-import { Example } from "@monorepo-template/core/example";
+import { Example } from "@drain-monitoring/core/example";
 
 console.log(`${Example.hello()} Linked to ${Resource.MyBucket.name}.`);
